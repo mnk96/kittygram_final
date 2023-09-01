cb9e6ae5628d84a646f4a8298e6ced2092395f7d
Наталья Мищенко <mnk96@yandex.ru>
2023-09-01 18:32:13 +0300

    Test new change 8

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
 # DEBUG = True
 
 # ALLOWED_HOSTS = []
